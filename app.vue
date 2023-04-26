c0d950544011922cb03050ffe7769e8bdef7e5ef
Simone Romano <me@simoneromano.com>
2023-04-26 21:16:02 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <TwentyTwenty
+    before="//placehold.it/600x200/E8117F/FFFFFF"
+    after="//placehold.it/600x200/CCCCCC/FFFFFF"
+  />
+</template>
+
+<script setup>
+import "vue-twentytwenty/dist/vue-twentytwenty.css";
+import TwentyTwenty from "vue-twentytwenty";
+</script>
